57f60b58e0f319c723adceb3d10a943b48b4a96c
timofey1826 <105168955+timofey1826@users.noreply.github.com>
2024-02-25 20:00:33 +0400

    db_conn

--- django_back/django_back/settings.py
+++ django_back/django_back/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'first',
+    'first.apps.FirstConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "trydozatrati",
+        "USER": "postgres",
+        "PASSWORD": "123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
--- django_back/first/models.py
+++ django_back/first/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class JobTitle(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=800)
+
+
+class Task(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.PROTECT)
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=800)
+
+
+class Users(models.Model):
+    job_title_id = models.ForeignKey(JobTitle, on_delete=models.PROTECT)
+    age = models.IntegerField(default=0)
+    first_name = models.CharField(max_length=800)
+    last_name = models.CharField(max_length=800)
+    father_name = models.CharField(max_length=800)
+
+
+class WorkOnTask(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT)
+    project_id = models.ForeignKey(Project, on_delete=models.PROTECT)
+    task_id = models.ForeignKey(Task, on_delete=models.PROTECT)
+    work_date = models.DateField()
+    work_time = models.IntegerField(default=0)
+
