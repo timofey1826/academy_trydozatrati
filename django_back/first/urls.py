e6cf6c999e5660f8cd2d8e1306436c95d290ac69
timofey1826 <105168955+timofey1826@users.noreply.github.com>
2024-03-01 19:14:39 +0400

    filter_work

--- django_back/first/urls.py
+++ django_back/first/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("users",views.usersManaging),
-    path("get_job_title/1",views.getJobTitle)
+    path("get_job_title/<pk>",views.getJobTitle)
 ]
--- django_back/first/views.py
+++ django_back/first/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 
-from .models import User, JobTitle
+from .models import User, JobTitle, Project
 from .serializer import UsersSerializer, JobTitleSerializer
 
 
@@ -30,13 +30,13 @@ def usersManaging(request):
 
 @api_view(['GET', 'POST', 'DELETE'])
 def getJobTitle(request,pk):
-
-
+    tutorial = JobTitle.objects.filter(id = pk)
+    print(tutorial)
     if request.method == 'GET':
         try:
-            tutorial = JobTitle.objects.get(pk=pk)
-            tutorials_serializer = JobTitleSerializer(tutorial, many=True)
-            return JsonResponse(tutorials_serializer.data, safe=False)
+            tutorial_serializer = JobTitleSerializer(tutorial[0])
+            print(tutorial_serializer.data)
+            return JsonResponse(tutorial_serializer.data, safe=False)
         except JobTitle.DoesNotExist:
             return JsonResponse({'message': 'The tutorial does not exist'}, status=404)
 
@@ -47,3 +47,13 @@ def getJobTitle(request,pk):
             tutorial_serializer.save()
             return JsonResponse(tutorial_serializer.data, status=200)
         return JsonResponse(tutorial_serializer.errors, status=400)
+
+@api_view(['GET', 'POST', 'DELETE'])
+def ProjectsCRUD(request,pk):
+    project = Project.objects.get(pk=pk)
+    if request.method == 'GET':
+        try:
+            project_serializer = JobTitleSerializer(project)
+            return JsonResponse(project_serializer.data, safe=True)
+        except JobTitle.DoesNotExist:
+            return JsonResponse({'message': 'The tutorial does not exist'}, status=404)
