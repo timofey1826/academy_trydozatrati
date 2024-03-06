2f98931a37fa3fb8c71e89f7e1a7bde472b720e9
timofey1826 <105168955+timofey1826@users.noreply.github.com>
2024-03-06 20:17:31 +0400

    2

--- django_back/db_CRUD/urls.py
+++ django_back/db_CRUD/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views, specific_commands
+from .views import TokenObtainPairViewModified
 
 urlpatterns = [
     path("user/<id>",views.user_managing),
@@ -9,5 +10,6 @@ urlpatterns = [
     path("task/<id>",views.task_managing),
 
     path("all_task", specific_commands.get_all_tasks),
-    path("all_user_task/<user_id>", specific_commands.get_all_user_task)
+    path("all_user_task/<user_id>", specific_commands.get_all_user_task),
+    path('token/', TokenObtainPairViewModified.as_view(), name='token_obtain_pair')
 ]
